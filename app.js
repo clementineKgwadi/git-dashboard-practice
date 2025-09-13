83af24075395563f0c1985a374d0a44e8ea410cf
Clementine <clementinekgwadi43@gmail.com>
2025-09-13 23:36:35 +0200

    improve error handling with response.ok check

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ async function fetchData() {
   try {
     const response = await fetch(API_ENDPOINT);
 
+    if(!response.ok){
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
     const data = await response.json();
 
     dataContainer.innerHTML = `<strong> Data Loaded: </strong> ${data.message}`;
-  } catch (error) {}
+  } catch (error) {
+    dataContainer.innerHTML = `<span class = "error"> Failed to load data. </span>`;
+    console.error('Fetch Error:', error);
+  }
 }
 loadBtn.addEventListener("click,", fetchData);
