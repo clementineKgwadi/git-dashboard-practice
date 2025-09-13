ee01b3fe8093bd182423f34f7ce779c63e4724e5
Clementine <clementinekgwadi43@gmail.com>
2025-09-13 21:36:28 +0200

    Initial setup

---
+++ app.js
@@ -0,0 +1,14 @@
+const API_ENDPOINT = "https://api.example.com/data";
+const dataContainer = document.getElementById('data-container');
+const loadBtn = document.getElementById('load-data-btn');
+
+function fetchData(){
+    fetch(API_ENDPOINT).then(response => response.json())
+    .then(data => {
+        dataContainer.innerHTML = `<strong> Data Loaded: </strong> ${data.message}`;
+    }).catch(error => {
+        dataContainer.innerHTML = `<span class = "error"> Failed to load data. </span>`;
+        console.error('Fetch Error:', error);
+    });
+}
+loadBtn.addEventListener('click,', fetchData);
